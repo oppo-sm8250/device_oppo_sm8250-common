7bf5dd1758ff12361ba6e59aee24202409102f8a
Isaac Chen <tingyi364@gmail.com>
2021-08-23 03:54:20 +0200

    OP4A79: Add PocketMode
     * Modified from https://github.com/LineageOS/android_device_xiaomi_jason/tree/df0cd8a1b9cb17a9199f2026b3eb87761c529b50/pocketmode
    
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- device.mk
+++ device.mk
@@ -349,6 +349,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Pocket mode
+PRODUCT_PACKAGES += \
+    OPPOPocketMode
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
